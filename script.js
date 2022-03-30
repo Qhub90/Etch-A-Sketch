0cdc7684d5af3dda5f0f10017d7332e4f2d89ffc
Qinux <qdaman90@gmail.com>
2022-03-30 13:48:46 -0500

    Added a working reset button Also created a button that takes in a number 1-100 and creates a grid with that number.

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-let grid = document.querySelector('.grid');
+const grid = document.querySelector(".grid");
+const clearBtn = document.querySelector("#clear");
+const promptBtn = document.querySelector("#prompt");
 
 const colorChange = (e) => {
-    e.target.style.backgroundColor='green'
-    console.log(e.target.style);
-    }
+  e.target.style.backgroundColor = "green";
+};
 
-for(let i = 1; i <= 256; i ++){
-    const div = document.createElement('div');
-    div.addEventListener('click', colorChange)
+const createGrid = (x) => {
+  let squaredPixel = x * x;
+
+  for (let i = 1; i <= squaredPixel; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", colorChange);
     grid.appendChild(div);
+  }
 
-}
+  grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${x}, 1fr)`;
+};
 
+const reset = () => {
+  const sketchPad = document.querySelectorAll("div.grid > div");
+  for (let pixel of sketchPad) {
+    pixel.style.backgroundColor = "white";
+  }
+};
+
+clearBtn.addEventListener("click", reset);
+
+const gridSize = () => {
+  let input = prompt("Grid Size");
+
+  if (input > 100) {
+    return console.log("Too big");
+  } else {
+    grid.innerHTML = "";
+    createGrid(input);
+  }
+};
+
+promptBtn.addEventListener("click", gridSize);
+
+createGrid(16);
